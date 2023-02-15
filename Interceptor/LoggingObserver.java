11f0502242ad79f9ca20fe4f43ef9f93e9116470
TitoEt <19248547@studentmail.ul.ie>
2023-02-15 16:03:15 +0000

    WeatherStation application + interceptor construct

---
+++ Interceptor/LoggingObserver.java
@@ -0,0 +1,23 @@
+package Interceptor;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class LoggingObserver implements Observer {
+    private Observer wrappedObserver;
+
+    public LoggingObserver(Observer observer) {
+        this.wrappedObserver = observer;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        log("Before update");
+        wrappedObserver.update(o, arg);
+        log("After update");
+    }
+
+    private void log(String message) {
+        System.out.println("LoggingObserver: " + message);
+    }
+}
